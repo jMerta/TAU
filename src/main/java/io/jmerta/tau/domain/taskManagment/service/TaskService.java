4a11d371393373de2b571a5f82eaf4d788261f3e
Jakub Merta <jakubm@lsnova.pl>
2018-06-17 19:39:27 +0200

    test

--- src/main/java/io/jmerta/tau/domain/taskManagment/service/TaskService.java
+++ src/main/java/io/jmerta/tau/domain/taskManagment/service/TaskService.java
@@ -1,8 +1,12 @@
 package io.jmerta.tau.domain.taskManagment.service;
 
+import io.jmerta.tau.domain.taskManagment.entity.Task;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
 
+    public void createNewTask(Task task){
+
+    }
 }
