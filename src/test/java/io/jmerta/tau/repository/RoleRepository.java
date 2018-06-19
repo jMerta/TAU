8a1ad28ba3bd4060b71ab27f651fd45224cd28c1
jMerta <mer.kuba@gmail.com>
2018-06-19 22:34:31 +0200

    tests before implementation

--- src/test/java/io/jmerta/tau/repository/RoleRepository.java
+++ src/test/java/io/jmerta/tau/repository/RoleRepository.java
@@ -2,6 +2,7 @@ package io.jmerta.tau.repository;
 
 import io.jmerta.tau.TauApplication;
 import io.jmerta.tau.config.DataConfig;
+import io.jmerta.tau.domain.accountManagment.entity.Role;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
@@ -9,6 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @RunWith(SpringRunner.class)
 @MybatisTest(excludeAutoConfiguration = {AutoConfigureTestDatabase.class, SpringBootTest.class})
@@ -23,6 +29,10 @@ public class RoleRepository {
 
     @Test
     public void CheckIfAllRolesExist(){
+        List<Role> roleList = roleRepository.getAllRoles();
 
+        assertThat(roleList).isNotEmpty();
+        assertThat(roleList.size()).isEqualTo(2);
+        assertThat(roleList).anyMatch(role -> role.getCode().equalsIgnoreCase("Admin"));
     }
 }
