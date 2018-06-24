df2b640bae2e4f279ef94f9ffd2341245763dd91
Jakub Merta <jakubm@lsnova.pl>
2018-06-24 20:45:15 +0200

    board api

--- src/main/java/io/jmerta/tau/domain/taskManagment/transport/BoardController.java
+++ src/main/java/io/jmerta/tau/domain/taskManagment/transport/BoardController.java
@@ -1,9 +1,11 @@
 package io.jmerta.tau.domain.taskManagment.transport;
 
+import io.jmerta.tau.domain.taskManagment.entity.Board;
 import io.jmerta.tau.domain.taskManagment.service.ManageBoard;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/board")
@@ -16,7 +18,23 @@ public class BoardController {
         this.manageBoard = manageBoard;
     }
 
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public ResponseEntity<Board> createNewBoard(@RequestBody Board board) {
+        manageBoard.createNewBoard(board);
 
-    
+        return new ResponseEntity<>(board, HttpStatus.OK);
+    }
 
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity deleteBoard(@PathVariable("id") long id) {
+        manageBoard.deleteBoard(id);
+        return new ResponseEntity(null, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public ResponseEntity<Board> editBoard(@RequestBody Board board) {
+        manageBoard.updateBoard(board);
+
+        return new ResponseEntity<>(board, HttpStatus.OK);
+    }
 }
--- src/main/java/io/jmerta/tau/domain/taskManagment/transport/CategoryController.java
+++ src/main/java/io/jmerta/tau/domain/taskManagment/transport/CategoryController.java
@@ -1,9 +1,20 @@
 package io.jmerta.tau.domain.taskManagment.transport;
 
+import io.jmerta.tau.domain.taskManagment.service.ManageCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/category")
 public class CategoryController {
+
+    private ManageCategory manageCategory;
+
+    @Autowired
+    public CategoryController(ManageCategory manageCategory) {
+        this.manageCategory = manageCategory;
+    }
+
+
 }
